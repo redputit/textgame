ed7602cfed0d66d76c4d7ebd16716df73463ddd2
redputit <greaterputit@gmail.com>
2018-01-05 22:36:49 +0900

    Onesのメソッド追加修正とゲームループ用クラスactionを追加

--- src/text/Ones.java
+++ src/text/Ones.java
@@ -5,15 +5,18 @@ import java.util.Random;
  * PC含むキャラクタの生成
  */
 public class Ones {
+	
 	int i;
 	final short HumanBlood = 1000;//cc
 	
+	byte arraynumber;
 	Random rand = new Random();
 	boolean servive;
 	String name;
 	short speed;
 	short Mbody;//要考慮
 	short body;
+	short protection;
 	short[] blood = new short[5];//0:fire 1:water 2:wind 3:earth 4:ether
 	byte type; //0:human 1:phlebotomy 2:lover 3:butterfly
 	
@@ -21,52 +24,9 @@ public class Ones {
 	short[][] recipe = new short[4][5];
 
 	
-	public Ones(String s,int b,int t){
 
-		servive = true;
-		name = s;
-		type = (byte)t;	
-		body = Mbody = (short)b;
-		
-		switch(type) {
-			case 0:
-				for(i =0; i< 5 ;i++ ) {
-					blood[i] = HumanBlood;
-				}
-				break;
-				
-			case 1:
-				for(i =0; i< 5 ;i++ ) {
-					blood[i] = (short)rand.nextInt(HumanBlood * 4);
-					if(Mbody < blood[i]) {
-						body = Mbody = blood[i];
-					}
-				}
-				break;
-				
-			case 2:
-				for(i =0; i< 5 ;i++ ) {
-					blood[i] = 0;
-				}
-				break;
-				
-			case 3:
-				for(i =0; i< 5 ;i++ ) {
-					blood[i] = (short)rand.nextInt(HumanBlood * 10);
-					if(Mbody < blood[i]) {
-						body = Mbody = blood[i];
-					}
-				}
-				break;
-				
-		}
-	}
-
-	public Ones() {//for enemy
-
-	}
-	
-	void setStates() {
+	void setStates(int n){
+		arraynumber = (byte)n;
 		servive = true;
 		name = null;
 		type = (byte)rand.nextInt(4);	
@@ -106,6 +66,7 @@ public class Ones {
 		}
 
 	}
+	
  	void makeRecipe(int n, String s, int[] cost){//配列は5つまで……　指定したい
 		recipeName[n][32] = s;
 		for(i =0; i< 5 ;i++ ) {
@@ -115,26 +76,24 @@ public class Ones {
 		
 	}
 
-	short magic(int n) {
+ 	void magic(int[] cost) {
+
 		short damege = 0;
-		
-		return damege;
+		living(damege);
 	}
-	short magic(int[] cost) {
-		short damege = 0;
-		return damege;
+ 	
+	void melee(int power) {
+		short damege = (short) (power - protection);
+		living(damege);
 	}
-	short melee(int power) {
-		short damege = 0;
-		return damege;
-	}
-	boolean living(short damege) {
-		boolean l;
+	
+	void living(short damege) {
 		body -= damege;
-		l = body > 0 ? true : false;
-		return l;
-		
+		servive = body > 0 ? true : false;
+		if(body <= 0 || arraynumber == 0)
+				System.out.println("GameOver");
 	}
+
 }
 
 
---
+++ src/text/action.java
@@ -0,0 +1,17 @@
+package text;
+
+public class action{
+	
+	public action() {
+		Ones[] characters = new Ones[32];
+		for(int i = 0; i<32;i++) {
+			characters[i].setStates(i);
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
